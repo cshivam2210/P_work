3a9424f745b12a7f452464253682a8c6ad9c961a
Shivam <cshivam2210@gmail.com>
2021-01-22 18:20:51 +0530

    Sum_of_ele

---
+++ Sum_of_ele.py
@@ -0,0 +1,6 @@
+Total_ele = 0
+list_a = [11, 5, 17, 18, 23] 
+n = len(list_a)
+for ele in range(0,n):
+    Total_ele = Total_ele + list_a[ele]
+print("Sum of all elements in given list: ", Total_ele)
